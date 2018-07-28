38eed39142b400e199107981145d328b5bc6d063
bodnarrr <a.bodnarrr@gmail.com>
2018-07-28 16:10:03 +0300

    Initial commit

---
+++ SwiftyProteins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyProteins
+//
+//  Created by Andriy BODNAR on 7/28/18.
+//  Copyright © 2018 Andriy BODNAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+}
+
