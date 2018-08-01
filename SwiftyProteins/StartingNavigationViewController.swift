917b8c91f093ac18435eddb834739de69effa8ee
Andrii Bodnar <andrii.bodnar@sannacode.com>
2018-08-01 11:15:12 +0300

    - fixed checking data after request - fixed constraints in tableViewCell - todo: show activityIndicator

--- SwiftyProteins/StartingNavigationViewController.swift
+++ SwiftyProteins/StartingNavigationViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class StartingNavigationViewController: UINavigationController {
 
-    override func viewDidLoad() {
+
+	override func viewDidLoad() {
         super.viewDidLoad()
     }
 	
